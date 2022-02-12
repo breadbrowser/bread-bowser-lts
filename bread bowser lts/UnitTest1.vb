dfbc0d1b9f474f131ab2dd867f2510c319943394
Matthew nuts <For360ffg@outlook.com>
2022-02-11 23:27:41 -0500

    Add project files.

---
+++ bread bowser lts/UnitTest1.vb
@@ -0,0 +1,18 @@
+Imports NUnit.Framework
+
+Namespace bread_bowser_lts
+
+    Public Class Tests
+
+        <SetUp>
+        Public Sub Setup()
+        End Sub
+
+        <Test>
+        Public Sub Test1()
+            Assert.Pass()
+        End Sub
+
+    End Class
+
+End Namespace
